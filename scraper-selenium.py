d47ed7a3268c6544bde46babda3a21516ed52c3f
Maarten Wolfsen <maartenwolfsen@gmail.com>
2021-11-12 07:58:19 +0100

    Add geckodriver executable

--- scraper-selenium.py
+++ scraper-selenium.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-driver = webdriver.Firefox(executable_path=r'C:\Program Files (x86)\geckodriver.exe')
+driver = webdriver.Firefox(executable_path=r'.\res\geckodriver.exe')
 driver.get('https://www.mediamarkt.nl/nl/product/_legend-of-zelda-link-s-awakening-nintendo-switch-1623975.html')
 
 time.sleep(3)
@@ -18,5 +18,11 @@ if (len(cookiePopup) > 0):
 productPrice = driver.find_elements_by_xpath('//meta[@itemprop="price"]')[0].get_attribute("content")
 
 if (float(productPrice) > 50.0):
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.price-button"))).click()
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a#basket-flyout-cart"))).click()
+    print("Price is above 50!")
+
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "div.price-button"))
+    ).click()
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "a#basket-flyout-cart"))
+    ).click()
